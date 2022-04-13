2f6943d1ee2981597748088f75a861f06ca0bd9c
varad4545 <68903734+varad4545@users.noreply.github.com>
2022-04-13 22:34:29 +0530

    Add files via upload

---
+++ Assignment13_GIPHY_HOOKS/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+
+
+import GiphyPageHook from './components/GiphyPageHooks';
+
+
+
+
+function App() {
+
+  
+  return (
+ 
+  <GiphyPageHook/>
+  
+  
+  );
+}
+export default App;
