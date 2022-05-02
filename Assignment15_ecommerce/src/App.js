d1931ade5960e769aff7b61f1b5796f82c26bd56
varad4545 <68903734+varad4545@users.noreply.github.com>
2022-05-02 16:50:44 +0530

    Add files via upload

---
+++ Assignment15_ecommerce/src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+
+import Header from './components/Header';
+import ProductComponents from './components/ProductComponents';
+import ProductDetails from './components/ProductDetails';
+import ProductListing from './components/ProductListing';
+// function App() {
+//   return (
+//     <div className="App">
+
+
+
+  
+
+//         <Router>
+//           <Header/>
+//           <Switch>
+//               <Route exact path='/' component={ProductListing}/>
+//               <Route path="/product/:productId" component={ProductDetails}/>
+//               <Route>404 Not Found</Route>
+//           </Switch>
+
+//         </Router> 
+//     </div>
+//   );
+// }
+
+// export default App;
+function App() {
+  return (
+    <div className="App">
+
+
+       
+
+    </div>
+  );
+}
+
+export default App;
+
